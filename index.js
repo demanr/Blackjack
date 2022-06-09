641bfa58094a33542acc49fae5ef2fd08c5ba8c1
Rachelle DeMan <demanr@mcmaster.ca>
2022-06-08 23:44:57 -0400

    Added CSS Decor & starting text

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // TBA
+let firstCard =  Math.floor(Math.random() * 9) + 2
+let secondCard = 8 // Math.floor(Math.random() * 9) + 2
+let sum = firstCard + secondCard + 4
+let hasBlackJack = false
+let isAlive = true
+
+let message = ""
+
+if (sum <= 20) {
+    message = "Draw a card?"
+} else if (sum === 21) {
+    message = "You got Blackjack!"
+    hasBlackJack = true
+} else {
+    message = "You are out."
+    isAlive = false
+}
+
+console.log(hasBlackJack)
+console.log(isAlive)
+console.log(message)
